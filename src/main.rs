4b0da75c0038a249c439e8121ce1a5dbdde3d37f
gsc_prukol <gsc.prukol@gmail.com>
2026-07-25 22:58:57 +0300

    Fix build

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use macroquad::prelude::*;
-
-#[macroquad::main("Downbeat")]
-async fn main() {
-    loop {
-        clear_background(DARKPURPLE);
-        next_frame().await
-    }
+fn main() {
+    print!("Hello word!");
 }
